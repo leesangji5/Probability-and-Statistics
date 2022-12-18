a3ff3e349f71355568a8becabebb90740a05bb9b
sangji <lsgsh211@gmail.com>
2022-12-18 19:34:31 +0900

    add .

--- 2_static_graph/poker.py
+++ 2_static_graph/poker.py
@@ -2,13 +2,14 @@ import random
 import matplotlib.pyplot as plt
 # 433160번당 1회
 
-n = 43316000
+n = 433160000
 result = [0, 0, 0, 0, 0]
 for j in range(5):
     for i in range(n):
         a = random.sample(range(1, 53), 6)
         if 1 in a and 2 in a and 3 in a and 4 in a and 5 in a:
             result[j] += 1
+    print(result[j])
 
 
 label = ['1', '2', '3', '4', '5']
