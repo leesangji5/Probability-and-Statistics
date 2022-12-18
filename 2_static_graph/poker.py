c010702bf76d4288dbe2b6689d4fc550a1feb464
sangji <lsgsh211@gmail.com>
2022-12-18 19:21:41 +0900

    add .

--- 2_static_graph/poker.py
+++ 2_static_graph/poker.py
@@ -1,11 +1,17 @@
 import random
+import matplotlib.pyplot as plt
 # 422160번당 1회
 
-n = 4331600
-result = 0
-for i in range(n):
-    a = random.sample(range(1, 53), 6)
-    if 1 in a and 2 in a and 3 in a and 4 in a and 5 in a:
-        result += 1
+n = 43316000
+result = [0, 0, 0, 0, 0]
+for j in range(5):
+    for i in range(n):
+        a = random.sample(range(1, 53), 6)
+        if 1 in a and 2 in a and 3 in a and 4 in a and 5 in a:
+            result[j] += 1
 
-print(result)
+
+label = ['1', '2', '3', '4', '5']
+plt.bar(label, result, color='blue')
+plt.axhline(y=n/433160, color='r', linestyle='-')
+plt.show()
