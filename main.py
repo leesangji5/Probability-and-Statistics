8ca9502a9af421b5854f2ee19bafd478da3d8643
sangji <lsgsh211@gmail.com>
2022-12-26 22:44:35 +0900

    add .

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
+from random import randint
 import matplotlib.pyplot as plt
 
-label = ['1', '2', '3', '4', '5']
-result = [999, 1018, 956, 1040, 1027]
+dice_num = 10
+n = 1000000
+result = [0]*(dice_num*6-dice_num+1)
+for i in range(n):
+    a = 0
+    for j in range(dice_num):
+        a += randint(1, 6)
+    result[a-dice_num] += 1
+
+print(result)
+label = []
+for i in range(len(result)):
+    label.append(i+dice_num)
+
 plt.bar(label, result, color='blue')
-plt.axhline(y=1000, color='r', linestyle='-')
+plt.xlabel('Number')
+plt.ylabel('Frequency')
+plt.title('Frequency of Number')
 plt.show()
